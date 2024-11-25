1a5e8daaae34144f48e40c0badba726dbf10f9f1
Puneet56 <puneetdhiman56@gmail.com>
2024-11-25 22:13:08 +0530

    Refactored code

--- src/breakout.c
+++ src/breakout.c
@@ -12,6 +12,10 @@
 // Bricks (Row 3)	Lemon Yellow	#ffff66
 // Special Effects	Neon Pink	#ff007f
 // Bonus Points	Gold	#ffd700
+// Bricks can change color as they're hit, e.g., Bright Red → Bright Orange →
+// Lemon Yellow → Destroyed.
+// Glow or neon effects can add depth; simulate this
+// by applying a halo effect around elements.
 
 Color BACKGROUND = {0x0d, 0x1b, 0x2a, 255};
 Color PADDLE = {0x39, 0xff, 0x14, 255};
@@ -19,6 +23,10 @@ Color BALL = {0xff, 0xdd, 0x00, 255};
 Color BRICKS = {0xff, 0x45, 0x00, 255};
 Color SPECIAL = {0xff, 0x00, 0x7f, 255};
 Color BONUS = {0xff, 0xd7, 0x00, 255};
+
+typedef struct Game {
+  bool over;
+} Game;
 
 typedef struct Ball {
   Vector2 pos;
@@ -32,18 +40,18 @@ typedef struct Paddle {
   Vector2 vel;
 } Paddle;
 
-typedef struct Game {
-  bool game_over;
-} Game;
+typedef struct Brick {
+  bool isBroken;
+} Brick;
 
-int height = 600;
-int width = 800;
 int main() {
-  InitWindow(width, height, "Hello raylib");
-  SetTargetFPS(60);
+  //--------------------------------------------------------------------------------------
+  // Initialization
 
+  int height = 600;
+  int width = 800;
   Game game = {
-      .game_over = false,
+      .over = false,
   };
 
   Ball ball = {
@@ -52,49 +60,28 @@ int main() {
       .speed = (Vector2){4, 4},
   };
   Paddle paddle = {
-      .pos = (Vector2){width / 2, height - 10},
+      .pos = (Vector2){(float)width / 2, height - 10},
       .size = {150, 10},
       .vel = 10,
   };
 
+  InitWindow(width, height, "Breakout");
+  SetTargetFPS(60);
   while (!WindowShouldClose()) {
     BeginDrawing();
-    if (game.game_over) {
+    if (game.over) {
       DrawText("Game over", 100, 100, 40, WHITE);
+      // Reload
       if (IsKeyDown(KEY_R)) {
-        game.game_over = false;
+        game.over = false;
         ball.pos.x = 100;
         ball.pos.y = 100;
       }
     } else {
-      ClearBackground(BACKGROUND);
-      if (ball.pos.x > width - ball.r || ball.pos.x < ball.r) {
-        ball.speed.x *= -1;
-      }
-
-      if (ball.pos.y < ball.r || ball.pos.y > height - ball.r) {
-        ball.speed.y *= -1;
-      }
-
-      if (ball.pos.y > height - ball.r) {
-        game.game_over = true;
-      }
-
-      if (CheckCollisionCircleRec(ball.pos, ball.r,
-                                  (Rectangle){paddle.pos.x, paddle.pos.y,
-                                              paddle.size.x, paddle.size.y})) {
-        ball.speed.y *= -1;
-        ball.speed.x = (ball.pos.x - paddle.pos.x - paddle.size.x / 2) /
-                       (paddle.size.x / 2) * 10;
-      }
-
+      //--------------------------------------------------------------------------------------
+      // Movement
       ball.pos.x += ball.speed.x;
       ball.pos.y += ball.speed.y;
-      DrawCircle(ball.pos.x, ball.pos.y, ball.r, BALL);
-
-      DrawRectangle(paddle.pos.x, paddle.pos.y, paddle.size.x, paddle.size.y,
-                    PADDLE);
-
       if (IsKeyDown(KEY_L) && paddle.pos.x < width - paddle.size.x) {
         paddle.pos.x += paddle.vel.x;
       }
@@ -103,6 +90,51 @@ int main() {
         paddle.pos.x -= paddle.vel.x;
       }
     }
+
+    //--------------------------------------------------------------------------------------
+    // Draw
+    ClearBackground(BACKGROUND);
+    DrawCircle(ball.pos.x, ball.pos.y, ball.r, BALL);
+    DrawRectangle(paddle.pos.x, paddle.pos.y, paddle.size.x, paddle.size.y,
+                  PADDLE);
+
+    //--------------------------------------------------------------------------------------
+    // coliision walls and paddle
+    if (ball.pos.x > width - ball.r || ball.pos.x < ball.r) {
+      ball.speed.x *= -1;
+    }
+
+    if (ball.pos.y < ball.r || ball.pos.y > height - ball.r) {
+      ball.speed.y *= -1;
+    }
+
+    Rectangle paddleBoundryRectangle = {
+        .x = paddle.pos.x,
+        .y = paddle.pos.y,
+        .width = paddle.size.x,
+        .height = paddle.size.y,
+    };
+
+    bool isBallAndPaddleCollided =
+        CheckCollisionCircleRec(ball.pos, ball.r, paddleBoundryRectangle);
+
+    if (isBallAndPaddleCollided) {
+      ball.speed.y *= -1;
+      // reference =
+      // https://github.com/raysan5/raylib-games/blob/master/classics/src/arkanoid.c
+      ball.speed.x = (ball.pos.x - paddle.pos.x - paddle.size.x / 2) /
+                     (paddle.size.x / 2) * 15;
+    }
+
+    //--------------------------------------------------------------------------------------
+    // coliision bricks
+
+    //--------------------------------------------------------------------------------------
+    // State change
+    if (ball.pos.y > height - ball.r) {
+      game.over = true;
+    }
+
     EndDrawing();
   }
 
